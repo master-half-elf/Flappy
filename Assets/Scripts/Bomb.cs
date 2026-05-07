de7d1b2e5036bb6de72183786431bde1248897d8
DESKTOP-SFKN6FU\SIT <xdaiki@gmail.com>
2026-05-07 20:49:05 +0300

    ну такое себе изменение

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,7 +4,6 @@ public class Bomb : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        //Сomment
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
