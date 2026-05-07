9098fd7947f69260ef7d17956aa49e4a4647fc61
DESKTOP-SFKN6FU\SIT <xdaiki@gmail.com>
2026-05-07 20:48:05 +0300

    Подвинул игрока, добавил невероятный код

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,6 +4,7 @@ public class Bomb : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //Сomment
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
