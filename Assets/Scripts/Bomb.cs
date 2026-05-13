ae0da29c28179cd6c2a9b0e9e6cf976de8bfb9d9
DESKTOP-SFKN6FU\SIT <xdaiki@gmail.com>
2026-05-13 20:16:19 +0300

    Улучшил код

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,6 +4,8 @@ public class Bomb : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //Comment 
+
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
